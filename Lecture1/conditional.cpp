f81215532d98a401875281ce5ac1791eb50c3b53
jhapriya909 <thepriyajha8840@gmail.com>
2025-11-07 21:00:39 +0530

    d:\DSA Learn\Lecture1\conditional.cpp

---
+++ Lecture1/conditional.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a value:"<<endl;
+    cin>>a;
+
+    cout<<"Enter b value:"<<endl;
+    cin>>b;
+
+    if(a>b){
+        cout<<"A is greater:"<<endl;
+    }
+    if(b>a){
+        cout<<"B is greater:"<<endl;
+    }
+
+}
