eb578ddce0ffc6501dbe97694c7940d7780faba2
jhapriya909 <thepriyajha8840@gmail.com>
2025-11-08 12:26:04 +0530

    d:\DSA Learn\Lecture1\Practice3.cpp

---
+++ Lecture1/Practice3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =24 ;
+    if(a>20){
+        cout<<"Love"<<endl;
+    }
+    else if(a==0){
+        cout<<"Lovely"<<endl;
+    }
+ 
+
+    else{
+         cout<<"Babbar"<<endl;
+    }
+
+    cout<<a;
+}
