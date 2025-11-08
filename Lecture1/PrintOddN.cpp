b5057375199a1562aa46029fc2d2af9cda638077
jhapriya909 <thepriyajha8840@gmail.com>
2025-11-08 12:27:57 +0530

    d:\DSA Learn\Lecture1\PrintOddN.cpp

---
+++ Lecture1/PrintOddN.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a number:" << endl;
+    cin >> num;
+    if (num % 2 != 0)
+    {
+        cout << "Odd number" << endl;
+    }
+
+    else
+    {
+        cout << "Even number" << endl;
+    }
+}
