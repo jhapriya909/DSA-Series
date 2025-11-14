2af895b4d43916a576cc5b7f9672c3b983e89489
jhapriya909 <thepriyajha8840@gmail.com>
2025-11-14 21:55:23 +0530

    d:\DSA Learn\Lecture5\SwitchChar.cpp

---
+++ Lecture5/SwitchChar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch = '1';
+
+    cout << endl;
+
+    switch (ch)
+    {
+    case 1:
+        cout << "First" << endl;
+        break;
+
+    case '1':
+        cout << "second" << endl;
+        break;
+
+    default:
+        cout << "it is default" << endl;
+    }
+}
