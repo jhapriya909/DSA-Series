e9de3a65ec254b09c7f1ee9ace32c685c721a071
jhapriya909 <thepriyajha8840@gmail.com>
2025-11-29 23:19:41 +0530

    d:\DSA Learn\Lecture7\ReverseArray.cpp

---
+++ Lecture7/ReverseArray.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+
+    cout << endl;
+}
+int main()
+{
+
+    int arr[6] = {6, 5, 11, 7, 9, 8};
+    int brr[5] = {6, 5, 11, 7, 9};
+
+    reverse(arr, 6);
+
+    reverse(brr, 5);
+
+    printArray(arr, 6);
+
+    printArray(brr, 5);
+
+    return 0;
+}
