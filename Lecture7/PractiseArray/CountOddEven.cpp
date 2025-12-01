1c65ee60ea4e4484a5b810b5a42ea041d263f1d7
jhapriya909 <thepriyajha8840@gmail.com>
2025-12-01 09:14:51 +0530

    d:\DSA Learn\Lecture7\PractiseArray\CountOddEven.cpp

---
+++ Lecture7/PractiseArray/CountOddEven.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    int evenCount = 0, oddCount = 0;
+
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        if(arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+
+    cout << "Total Even numbers = " << evenCount << endl;
+    cout << "Total Odd numbers = " << oddCount << endl;
+
+    return 0;
+}
+ 
