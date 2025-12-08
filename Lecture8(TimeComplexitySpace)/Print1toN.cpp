e924c8e8ca089d95c4a383930a47e24446af2f10
jhapriya909 <thepriyajha8840@gmail.com>
2025-12-08 23:33:40 +0530

    d:\DSA Learn\Lecture8(TimeComplexitySpace)\Print1toN.cpp

---
+++ Lecture8(TimeComplexitySpace)/Print1toN.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
