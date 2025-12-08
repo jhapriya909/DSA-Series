e53b1f0bd1947d6427b730d26075ed906bebab48
jhapriya909 <thepriyajha8840@gmail.com>
2025-12-08 23:32:11 +0530

    d:\DSA Learn\Lecture8(TimeComplexitySpace)\ElementarrayLinearSearch.cpp

---
+++ Lecture8(TimeComplexitySpace)/ElementarrayLinearSearch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,  arr[n]  , key;
+bool found = false;
+for(int i = 0; i < n; i++){
+    if(arr[i] == key){
+        found = true;
+        break;
+    }
+}
+}
+// Time Complexity: O(n)
+// Space Complexity: O(1)
